994bb3ef9c21f03fae51a36442bf5788cc6304e6
Ivan Bokii <bokiyis@gmail.com>
2019-11-03 22:27:41 +0000

    Doing new stuff here

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 function thisIsGood() {
   let a = true;
+  let c = "hello";
   let b = 0;
 
+  // comment 4
   if (a) {
     b = 100;
+  } else if (c === "not hello") {
+    b = 142;
   }
 
   return 42;
