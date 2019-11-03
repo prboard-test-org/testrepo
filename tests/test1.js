96748b6dcebd7bea52ac06aa26e0e8934622e208
Ivan Bokii <bokiyis@gmail.com>
2019-11-03 20:02:04 +0000

    doing tests for a success build

--- tests/test1.js
+++ tests/test1.js
@@ -3,6 +3,7 @@ var assert = require('assert');
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
+      // just a stupid comment
       assert.equal([1, 2, 3].indexOf(4), -1);
     });
   });
