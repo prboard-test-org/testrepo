783bbc4c3e50d006bf3a516ee493bdacbb536293
Ivan Bokii <bokiyis@gmail.com>
2019-11-03 20:24:23 +0000

    another some commit

--- tests/test1.js
+++ tests/test1.js
@@ -4,7 +4,7 @@ describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
       // just a stupid comment
-      assert.equal([1, 2, 3].indexOf(4), -1);
+      assert.equal([1, 2, 3].indexOf(4), 1);
     });
   });
 });
