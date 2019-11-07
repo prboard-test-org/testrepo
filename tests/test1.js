5a426537a66813e399e27cfaade5303615a1df09
Ivan Bokii <bokiyis@gmail.com>
2019-11-07 21:32:14 +0000

    fixing fixing

--- tests/test1.js
+++ tests/test1.js
@@ -4,7 +4,6 @@ describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
       // just a stupid comment
-      console.log("hello this world");
       assert.equal([1, 2, 3].indexOf(4), -1);
     });
   });
