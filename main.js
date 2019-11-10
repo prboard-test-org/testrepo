8b6183fe0594e3068f334053337054bad535f4af
Ivan Bokii <bokiyis@gmail.com>
2019-11-10 14:16:39 +0000

    added a stupid comment to verify layout

---
+++ main.js
@@ -0,0 +1,16 @@
+function thisIsGood() {
+  let a = true;
+  let c = "hello";
+  let b = 0;
+
+  // comment 4
+  if (a) {
+    b = 100;
+  } else if (c === "not hello") {
+    b = 142;
+  }
+
+  // addign a comment
+
+  return 42;
+}
